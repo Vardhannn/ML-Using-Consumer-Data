1c5b70090c5092faff580d0a8e3c5740198b7e0d
Sri Vardhan <99092172+Vardhannn@users.noreply.github.com>
2022-05-31 23:08:22 +0530

    Update predict with accuracy.py

--- predict with accuracy.py
+++ predict with accuracy.py
@@ -1,18 +1,32 @@
-#run it in jupyter notebook
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier 
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-music_data = pd.read_csv('music.csv')
-X = music_data.drop(columns = ['genre'])
-y = music_data['genre']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1)
+# loading files using pandas library
+data = pd.read_csv("music.csv")
 
+# we spilt data into input(x) and output(y) data sets
+x = data.drop(columns="genre")
+y = data["genre"]
+
+# here we separate again for training and testing with the help of the function train_test_split 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+
+# decisiontreeclassifier is a ML algorithm which make all the insigits in our program
 model = DecisionTreeClassifier()
-model.fit(X_train, y_train)
+model.fit(x_train, y_train)
 
-predictions = model.predict(X_test)
+
+print("input data set")
+predictions = model.predict(x_test)
+print(x_test , end ="\n\n\n" )
+print("expected results")
+print(y_test, end="\n\n\n\n")
+print ("model derived results based on the trained data set")
+print(predictions, end="\n\n\n")
+
 
 score = accuracy_score(y_test, predictions)
-score 
+print("accuracy score upon observing expected results and ML predicts or derived results")
+score
